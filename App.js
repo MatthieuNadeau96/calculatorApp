183b707934456aa1e6d82994180d6d5e6a12115a
Matthieu <matthieunadeau96@gmail.com>
2019-02-12 13:11:37 -0500

    added buttonRow for proper spacing of the digitKeys

--- App.js
+++ App.js
@@ -14,16 +14,26 @@ export default class App extends Component {
         <View style={styles.answerDisplay}></View>
         <View style={styles.keypad}>
           <View style={styles.digitKeys}>
-            <TouchableOpacity style={styles.digitKey}><Text style={styles.digitKeyText}>0</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.digitKey}><Text style={styles.digitKeyText}>1</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.digitKey}><Text style={styles.digitKeyText}>2</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.digitKey}><Text style={styles.digitKeyText}>3</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.digitKey}><Text style={styles.digitKeyText}>4</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.digitKey}><Text style={styles.digitKeyText}>5</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.digitKey}><Text style={styles.digitKeyText}>6</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.digitKey}><Text style={styles.digitKeyText}>7</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.digitKey}><Text style={styles.digitKeyText}>8</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.digitKey}><Text style={styles.digitKeyText}>9</Text></TouchableOpacity>
+            <View style={styles.buttonRow}>
+              <TouchableOpacity style={styles.key}><Text style={styles.keyText}>0</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.key}><Text style={styles.keyText}>1</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.key}><Text style={styles.keyText}>2</Text></TouchableOpacity>
+            </View>
+            <View style={styles.buttonRow}>
+              <TouchableOpacity style={styles.key}><Text style={styles.keyText}>3</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.key}><Text style={styles.keyText}>4</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.key}><Text style={styles.keyText}>5</Text></TouchableOpacity>
+            </View>
+            <View style={styles.buttonRow}>
+              <TouchableOpacity style={styles.key}><Text style={styles.keyText}>6</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.key}><Text style={styles.keyText}>7</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.key}><Text style={styles.keyText}>8</Text></TouchableOpacity>
+            </View>
+            <View style={styles.buttonRow}>
+              <TouchableOpacity style={styles.key}><Text style={styles.keyText}>9</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.key}><Text style={styles.keyText}>.</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.key}><Text style={styles.keyText}>=</Text></TouchableOpacity>
+            </View>
           </View>
           <View style={styles.operationKeys}></View>
         </View>
@@ -47,17 +57,27 @@ const styles = StyleSheet.create({
   keypad: {
     flex: 7,
     flexDirection: 'row',
+    flexWrap: 'wrap',
   },
   digitKeys: {
     flex: 3,
     backgroundColor: 'white',
-    flexDirection: 'row',
   },
-  digitKey: {
+  key: {
     flex: 1,
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: '#8b85a6',
   },
-  digitKeyText: {
+  keyText: {
     fontSize: 40,
+  },
+  buttonRow: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
   operationKeys: {
     flex: 1,
